04a9f9d209d9968de82f9d0ffae7990f53c8a0b1
Aidan <your@email.com>
2019-09-03 21:24:39 +0000

    added in bootstrap

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -5,9 +5,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Container from 'react-bootstrap/Container'
 
 const Hello = props => (
-  <div>Hello {props.name}!</div>
+  <div>
+    <Jumbotron fluid>
+    <Container>
+      <h1>Hello {props.name}!</h1>
+      <p>
+        This is a modified jumbotron that occupies the entire horizontal space of
+        its parent.
+      </p>
+    </Container>
+  </Jumbotron>
+  </div>
 )
 
 Hello.defaultProps = {
@@ -17,6 +29,8 @@ Hello.defaultProps = {
 Hello.propTypes = {
   name: PropTypes.string
 }
+
+
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'pp'
+#why ___dir___ isntead of path
+LIQUOR_FILE = File.join(__dir__, 'nc_quarterly_pricing_.csv')
 
 org = Organization.find_or_create_by!(name: 'The Original')
 bar = Bar.find_or_create_by!(organization: org, name: 'Tipsy Toddlers')
 user = User.find_or_initialize_by(organization: org, email: 'aidan.mattrick@gmail.com')
 user.update!(first_name: 'Aidan', last_name: 'Mattrick', password: 'password', password_confirmation: 'password')
+
+liquors = []
+File.open(LIQUOR_FILE, 'rt') do |f|
+  header = %i[nc_code brand name age proof capacity cost cost2] 
+  is_first = true
+  f.each_line do |line|
+    if is_first
+      is_first = false
+      next
+    end
+    values = line.split(',')
+    values.map! { |value| value.sub(/\A"(.*)"\z/, '\1') }
+    pairs = header.zip(values)
+    hash = Hash[pairs].slice(:brand, :name, :capacity, :cost)
+    hash[:spirit] = 'Unknown'
+    if hash[:capacity].downcase.ends_with?('ml')
+      hash[:capacity] = hash[:capacity].sub(/ml\z/i, '').to_f
+    elsif hash[:capacity].downcase.ends_with?('l')
+      hash[:capacity] = hash[:capacity].sub(/ml\z/i, '').to_f * 1000
+    end
+    liquors << hash
+  end
+end
+pp liquors
