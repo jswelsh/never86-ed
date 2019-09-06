4310a00baa855d6a11ff3f9dfe5b82d5bb0b4d8f
jswelsh <james.sinclair.welsh@gmail.com>
2019-09-06 00:02:01 +0000

    add camera helper to catch picture from user phone. need to pick the selector to use, likely queryselector?, are we just using html,css with this js componet

---
+++ app/javascript/helpers/camera.js
@@ -0,0 +1,14 @@
+//export function camera() {
+//HTML DOM querySelector() Method
+/*The querySelector() method returns the first child element 
+  that matches a specified CSS selector(s) of an element.
+  
+  create css file an pseudo html
+
+  The MediaDevices interface provides access to connected 
+  media input devices like cameras and microphones, as well 
+  as screen sharing. In essence, it lets you obtain access 
+  to any hardware source of media data.
+  https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices
+  */  
+
