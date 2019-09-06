4684600c92b21ab3837cb265cb4f7cbeb1d264f6
jswelsh <james.sinclair.welsh@gmail.com>
2019-09-06 02:33:20 +0000

    add camera component, need to grab input and send to back end via axios?

--- app/javascript/components/Camera.js
+++ app/javascript/components/Camera.js
@@ -4,25 +4,36 @@ import 'react-html5-camera-photo/build/css/index.css';
 //npm install --save react-html5-camera-photo
 //yarn add react-html5-camera-photo
  
-class App extends Component {
-  onTakePhoto (dataUri) {
-    // Do stuff with the dataUri photo...
+class Camera extends Component {
+  onTakePhoto (photo) {
+    // Do stuff with the photo photo...
     console.log('took photo - components/camera');
   }
 
   onCameraError (error) {
     console.error('Error:', error);
   }
+
+  onCameraStop () {
+    console.log('camera off');
+  }
  
   render () {
     return (
-      <div className="App">
+      <div className="Camera">
         <Camera
-          onTakePhoto = { (dataUri) => { this.onTakePhoto(dataUri); } }
+          onTakePhoto = { (photo) => { this.onTakePhoto(photo); } }
+          onCameraStart = { (stream) => { this.onCameraStart(stream); } }
+          onCameraStop = { () => { this.onCameraStop(); } }
+          idealResolution = {{width: 640, height: 480}}
+          imageType = {IMAGE_TYPES.JPG}
+          imageCompression = {0.97}
+          isMaxResolution = {false}
+          isImageMirror = {false}
         />
       </div>
     );
   }
 }
  
-export default App;
+export default Camera;
