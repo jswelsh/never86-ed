bbe2e887c9794757d542ece70c877bfc78a037d9
jswelsh <james.sinclair.welsh@gmail.com>
2019-09-06 01:46:05 +0000

    minor fix to nav bar

--- app/javascript/components/NavBar.js
+++ app/javascript/components/NavBar.js
@@ -1,18 +1,16 @@
 import React from "react"
 import Navbar from 'react-bootstrap/Navbar'
-import Jumbotron from 'react-bootstrap/Jumbotron'
+import Button from 'react-bootstrap/Button'
 
 export default function NavBar(props){
   
 
   return(
-    <Jumbotron fluid>
       <Navbar bg="dark" variant="dark" expand="xl">
-        <Navbar.Brand href="#home">Never86's -- currently in session_newjsx, move to nav later</Navbar.Brand>
+        <Navbar.Brand href="#home">Never86's</Navbar.Brand>
      {/*<Nav className="mr-auto">
         <Nav.Link href="#linkToSomeWhere">{props.name}</Nav.Link>
         </Nav> */}
       </Navbar>
-    </Jumbotron>
   )
 }
