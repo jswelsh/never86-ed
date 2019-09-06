55374fc918fd6c64ec4c05da189a7ed7db26f85b
jswelsh <james.sinclair.welsh@gmail.com>
2019-09-06 04:34:11 +0000

    add to render, button for take photo, button to stop camera, button to initialize feed. this will likely be removed as the component will initialize on render...depends on how it feels after using

--- app/javascript/components/Camera.js
+++ app/javascript/components/Camera.js
@@ -10,6 +10,41 @@ class Camera extends React.Component {
       dataUri: ''
     }
   }
+  
+
+
+  render () {
+    return (
+      <div>
+        <video
+          ref={this.videoRef}
+          autoPlay="true"
+          //this is experimental but could be useful
+          //autoPictureInPicture="true"
+        />
+        <button onClick={ () => {
+          //can switch to front via MODES.USER... n/v useful?
+          let facingMode = FACING_MODES.ENVIRONMENT;
+          //these need to be altered in light of TF requirements
+          let idealResolution = { width: 400, height: 400 };
+          this.startCamera(facingMode, idealResolution);
+        }}> Start environment facingMode resolution ideal 640 by 480 </button>
+
+        <button onClick={ () => {
+          this.takePhoto();
+        }}> Take photo </button>
+        <button onClick={ () => {
+          this.stopCamera();
+        }}> Stop </button>
+
+        <img
+          alt="Image"
+          src={this.state.dataUri}
+        />
+      </div>
+    );
+  }
+
 }
 
 export default Camera;
