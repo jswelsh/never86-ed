3852b3c101508ccdc08ebd58b64743a1789b42eb
jswelsh <james.sinclair.welsh@gmail.com>
2019-09-06 04:16:57 +0000

    add constructor, not sure if we really need live feed of video for camera, included anyways

--- app/javascript/components/Camera.js
+++ app/javascript/components/Camera.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import CameraPhoto, { FACING_MODES } from 'jslib-html5-camera-photo';
 
+class Camera extends React.Component {
+  constructor (props, context) {
+    super(props, context);
+    this.cameraPhoto = null;
+    this.videoRef = React.createRef();
+    this.state = {
+      dataUri: ''
+    }
+  }
+}
 
-
-export default App;
+export default Camera;
