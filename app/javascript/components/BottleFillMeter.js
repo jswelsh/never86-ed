c5b2fb42633e94be4958c547247ec1ab73b0458d
jswelsh <james.sinclair.welsh@gmail.com>
2019-09-06 08:08:36 +0000

    finish formating for bottle meter, need to modify incoming props to register full level/which bottle image to render

--- app/javascript/components/BottleFillMeter.js
+++ app/javascript/components/BottleFillMeter.js
@@ -1,13 +1,28 @@
 import React from "react";
-import { CircularProgressbar } from 'react-circular-progressbar';
+import { CircularProgressbarWithChildren, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 
 export default function BottleFillMeter(props){
 
 //turn to props.percentage    
-const percentage = props;
+const percentage = 66;
+//turn to props.bottleImage  
+//const bottleImage = props;
 return(
 
-<CircularProgressbar value={percentage} text={`${percentage}%`} />
+
+<CircularProgressbarWithChildren 
+  value={percentage}
+  styles={buildStyles({
+    pathColor: '#6ea4bf',
+    trailColor: '#6b2d5c',
+  })}>
+  {/* Put any JSX content in here that you'd like. It'll be vertically and horizonally centered. */}
+  <img style={{ width: 60, marginTop: -5 }} src="https://ww1.valuecellars.com.au/files/2016/06/682430611768-1.png" alt="doge" />
+  <div style={{ fontSize: 20, marginTop: -5 }}>
+    <strong>{percentage}%</strong> mate
+  </div>
+</CircularProgressbarWithChildren>
 )
 }
+
