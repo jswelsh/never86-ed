ff5e714ad9727c1d22b1c224dba6c48c7c784c73
jswelsh <james.sinclair.welsh@gmail.com>
2019-09-09 05:25:25 +0000

    Finish bottle card and bottlefill card, color scheme is to TBD. may have sizing issues later?

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,18 +3,18 @@ module ApplicationHelper
   def fillColor
     case 60
     when 80..100
-      "progress-bar progress-bar-striped progress-bar-animated--full"
+      "progress-bar progress-bar-striped progress-bar-animated --full"
     when 60..79
-      "progress-bar progress-bar-striped progress-bar-animated--sixtyUp"
+      "progress-bar progress-bar-striped progress-bar-animated --sixtyUp"
     when 40..59
-      "progress-bar progress-bar-striped progress-bar-animated--fortyUp"
+      "progress-bar progress-bar-striped progress-bar-animated --fortyUp"
     when 20..39
-      "progress-bar progress-bar-striped progress-bar-animated--twentyUp"
+      "progress-bar progress-bar-striped progress-bar-animated --twentyUp"
     when 0..19
-      "progress-bar progress-bar-striped progress-bar-animated--emptyUp"
+      "progress-bar progress-bar-striped progress-bar-animated --emptyUp"
     end
   end 
-
+  "progress-bar progress-bar-striped progress-bar-animated"
     def fillPercentage
       65
     end
